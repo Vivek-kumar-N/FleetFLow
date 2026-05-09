128ad4229a74e6e7446984551a930ce7a32fee14
somisettyshivani.lasya@ltm.com <somisettyshivani.lasya@ltm.com>
2026-05-09 17:18:38 +0530

    Maintenance Model and Repo

--- CarCaddy/carcaddy_Backend/carcaddy/src/main/java/com/carcaddy/entity/Maintenance.java
+++ CarCaddy/carcaddy_Backend/carcaddy/src/main/java/com/carcaddy/entity/Maintenance.java
@@ -1,5 +1,45 @@
 package com.carcaddy.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "maintenance")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Maintenance {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long maintenanceId;
+
+    @NotBlank(message = "Registration number is required")
+    private String registrationNumber;
+
+    @Enumerated(EnumType.STRING)
+    private MaintenanceType maintenanceType; 
+
+    private LocalDate scheduledDate;
+
+    private LocalDate completedDate;
+
+    private String description;
+
+    @Positive(message = "Cost must be positive")
+    private double cost;
+
+    private String performedBy; 
+
+    @Enumerated(EnumType.STRING)
+    private MaintenanceStatus status; 
 }
