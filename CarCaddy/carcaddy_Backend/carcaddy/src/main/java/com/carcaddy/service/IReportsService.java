518a277101d85b110293f5f40fdc9364ae5c7cda
vivek.kumar5@ltm.com <vivrk.kumar5@ltm.com>
2026-05-10 00:30:42 +0530

    Report Servoc

---
+++ CarCaddy/carcaddy_Backend/carcaddy/src/main/java/com/carcaddy/service/IReportsService.java
@@ -0,0 +1,21 @@
+package com.carcaddy.service;
+
+import com.carcaddy.entity.Customer;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public interface IReportsService {
+    List<Customer> getCustomersWithMaxBookings();
+    Map<String, Long> getCarsWithMinimalBookings();
+    Map<String, BigDecimal> getIncomeByCarModel();
+    BigDecimal getRevenueForPeriod(LocalDate startDate, LocalDate endDate);
+    Map<String, BigDecimal> getMaintenanceCostByCarModel();
+    Map<String, BigDecimal> getMaintenanceCostByPeriod(LocalDate startDate, LocalDate endDate);
+    Map<String, Object> getDashboardStatistics();
+    Map<String, Long> getCarUtilizationReport();
+    Map<String, Object> getCustomerLoyaltyAnalytics();
+    Map<String, BigDecimal> getProfitabilityReport();
+    Map<String, Object> getFleetHealthReport();
+}
