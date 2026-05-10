6a6abe6907586d05f065f4f0ddd8b7c712baf99f
harshit.agarwal2 <harshit.agarwal2@ltm.com>
2026-05-10 22:14:13 +0530

    APpUser Entity

---
+++ CarCaddy/carcaddy_Backend/carcaddy/src/main/java/com/carcaddy/entity/AppUser.java
@@ -0,0 +1,27 @@
+package com.carcaddy.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "app_user")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String role;
+
+    // getters & setters
+}
+    
