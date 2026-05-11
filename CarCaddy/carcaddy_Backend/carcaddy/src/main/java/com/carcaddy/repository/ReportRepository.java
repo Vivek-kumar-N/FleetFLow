e149d2b28ff952e1b01b3b63f32e57253f0ca6de
vivek.kumar5@ltm.com <vivek.kumar5@ltm.com>
2026-05-12 00:24:29 +0530

    change in report

--- CarCaddy/carcaddy_Backend/carcaddy/src/main/java/com/carcaddy/repository/ReportRepository.java
+++ CarCaddy/carcaddy_Backend/carcaddy/src/main/java/com/carcaddy/repository/ReportRepository.java
@@ -3,15 +3,17 @@ package com.carcaddy.repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
-public interface ReportRepository extends Repository<Object, Long> {
+import com.carcaddy.entity.Car;
+
+public interface ReportRepository extends Repository<Car, String> {
 
     @Query(value = """
-        SELECT 
-        (SELECT COUNT(*) FROM employee) as totalEmployees,
-        (SELECT COUNT(*) FROM car) as totalCars,
-        (SELECT COUNT(*) FROM customer) as totalCustomers,
-        (SELECT COUNT(*) FROM booking) as totalBookings,
-        (SELECT COUNT(*) FROM maintenance) as totalMaintenance
-        """, nativeQuery = true)
-    Object getDashboardStats();
+    SELECT 
+    (SELECT COUNT(*) FROM employee) as totalEmployees,
+    (SELECT COUNT(*) FROM car) as totalCars,
+    (SELECT COUNT(*) FROM customer) as totalCustomers,
+    (SELECT COUNT(*) FROM booking) as totalBookings,
+    (SELECT COUNT(*) FROM maintenance) as totalMaintenance
+    """, nativeQuery = true)
+Object getDashboardStats();
 }
